a3a6393db8346387a2585e6c90a5334b49a5e4cb
manuellito <manu.farcy@gmail.com>
2015-11-11 12:10:25 +0100

    Ajout du script Python pour communiquer avec l'utilisateur

---
+++ bin/quiesttu.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+def usage():
+    '''
+    TODO
+    '''
+    pass
+
+nom = raw_input("Qui est tu? ")
+
+if nom != "":
+    print "Bonjour %s" % nom
+else:
+    print "Bonjour anonyme. Tu semble timide"
+
+
+
